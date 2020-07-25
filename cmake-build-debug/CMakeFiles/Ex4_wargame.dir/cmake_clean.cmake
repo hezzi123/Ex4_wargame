499f7d7a976fb3fcbbab6131c8eedbebff7c2273
hezzi123 <47628021+hezzi123@users.noreply.github.com>
2020-07-26 00:07:35 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Ex4_wargame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex4_wargame.dir/main.cpp.obj"
+  "Ex4_wargame.exe"
+  "Ex4_wargame.exe.manifest"
+  "Ex4_wargame.pdb"
+  "libEx4_wargame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex4_wargame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
